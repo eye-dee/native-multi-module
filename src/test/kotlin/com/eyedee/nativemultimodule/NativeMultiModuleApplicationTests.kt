3766100a517f47d811f852168036f7893d85fcc5
eye-dee <drozdovigor1996@gmail.com>
2024-05-31 11:41:47 +0200

    Initial commit

---
+++ src/test/kotlin/com/eyedee/nativemultimodule/NativeMultiModuleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eyedee.nativemultimodule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NativeMultiModuleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
