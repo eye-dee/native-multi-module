b001dd89da6c0b76b4fa856417df8b14ac36faa4
eye-dee <drozdovigor1996@gmail.com>
2024-08-07 15:13:50 +0200

    Support transactional lock Remove unused resources registration

--- common/src/main/kotlin/com/eyedee/common/config/NativeRegistarHints.kt
+++ common/src/main/kotlin/com/eyedee/common/config/NativeRegistarHints.kt
@@ -5,10 +5,6 @@ import org.springframework.aot.hint.RuntimeHintsRegistrar
 
 class NativeRegistarHints: RuntimeHintsRegistrar {
     override fun registerHints(hints: RuntimeHints, classLoader: ClassLoader?) {
-        hints.resources().registerPattern("**/*.sql")
-        hints.resources().registerPattern("db/migration/*.sql")
-        hints.resources().registerPattern("db/migration/*")
-        hints.resources().registerPattern("**/db/migration/*.sql")
         hints.resources().registerPattern("**/*.yml")
         hints.resources().registerPattern("**/*.yaml")
         hints.resources().registerPattern("**/*.properties")
